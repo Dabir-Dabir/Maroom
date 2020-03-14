c63ba1cf8a94da33288663e1579bd9706bd60cfd
Lado Mikiashvili <lado.mikiashvili@gmail.com>
2020-03-14 12:43:11 +0400

    Add Project to Git

---
+++ src/components/error-indicator/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './error-indicator.scss';
+
+const ErrorIndicator = () => {
+    return (
+        <div className="error-indicator">
+            Не получилось получить данные!
+        </div>
+    );
+};
+
+export default ErrorIndicator;
